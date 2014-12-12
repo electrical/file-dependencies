5744c00250bb19b9dbfd286da6b6e2d43987103b
Richard Pijnenburg <richard@ispavailability.com>
2014-12-12 16:35:14 +0000

    Add more tests
    Fixes #2

---
+++ spec/file-dependencies_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+require 'tmpdir'
+require 'json'
+require 'file-dependencies'
+include WebMock::API
+describe FileDependencies do
+
+  describe '.download' do
+    after do
+      FileUtils.remove_entry_secure(tmpdir)
+      FileUtils.remove_entry_secure(target)
+      FileUtils.remove_entry_secure(file1)
+      FileUtils.remove_entry_secure(file2)
+      FileUtils.remove_entry_secure(file3)
+    end
+
+    let(:tmpdir) { Stud::Temporary.directory }
+    let(:target) { Stud::Temporary.directory }
+
+    let(:file1) { Assist.generate_tarball({'some/file' => 'content1', 'some/other/file' => 'content2', 'other' => 'content3'}) }
+    let(:file2) { Assist.generate_file('some_content') }
+    let(:file3) { Assist.generate_gzip('some_content_for_gzip') }
+
+    let(:sha1) { FileDependencies::File.calc_sha1(file1) }
+    let(:sha2) { FileDependencies::File.calc_sha1(file2) }
+    let(:sha3) { FileDependencies::File.calc_sha1(file3) }
+
+    let(:url1) { 'http://www.example.com/somefile1.tar.gz' }
+    let(:url2) { 'http://www.example.com/somefile2.txt' }
+    let(:url3) { 'http://www.example.com/somefile3.gz' }
+
+    let(:entries) { ['somefile2.txt', 'somefile3', 'some/file', 'some/other/file', 'other'] }
+
+    let(:files) { [ { 'url' => url1, 'sha1' => sha1 }, { 'url' => url2, 'sha1' => sha2 }, { 'url' => url3, 'sha1' => sha3 } ] }
+
+    it 'processes file list' do
+      stub_request(:get, url1).to_return(:body => File.new(file1), :status => 200)
+      stub_request(:get, url2).to_return(:body => File.new(file2), :status => 200)
+      stub_request(:get, url3).to_return(:body => File.new(file3), :status => 200)
+     
+      # we should not have any errors
+      expect{ FileDependencies.download(files, target, tmpdir) }.to_not(raise_error)
+
+      # check if we got all the expected files
+      found_files = Dir.glob(File.join(target, '**', '*')).reject { |entry| File.directory?(entry) }.sort
+      expect_files = entries.map { |k| ::File.join(target, k) }.sort
+      expect(found_files).to(eq(expect_files))
+      
+    end
+  end
+
+  describe '.process_vendor' do
+    after do
+      FileUtils.remove_entry_secure(tmpdir)
+      FileUtils.remove_entry_secure(target)
+      FileUtils.remove_entry_secure(file1)
+      FileUtils.remove_entry_secure(file2)
+      FileUtils.remove_entry_secure(file3)
+    end
+
+    let(:tmpdir) { Stud::Temporary.directory }
+    let(:target) { Stud::Temporary.directory }
+
+    let(:file1) { Assist.generate_tarball({'some/file' => 'content1', 'some/other/file' => 'content2', 'other' => 'content3'}) }
+    let(:file2) { Assist.generate_file('some_content') }
+    let(:file3) { Assist.generate_gzip('some_content_for_gzip') }
+
+    let(:sha1) { FileDependencies::File.calc_sha1(file1) }
+    let(:sha2) { FileDependencies::File.calc_sha1(file2) }
+    let(:sha3) { FileDependencies::File.calc_sha1(file3) }
+
+    let(:url1) { 'http://www.example.com/somefile1.tar.gz' }
+    let(:url2) { 'http://www.example.com/somefile2.txt' }
+    let(:url3) { 'http://www.example.com/somefile3.gz' }
+
+    let(:files) { [ { 'url' => url1, 'sha1' => sha1 }, { 'url' => url2, 'sha1' => sha2 }, { 'url' => url3, 'sha1' => sha3 } ].to_json }
+    let(:vendorfile) { File.write(File.join(target, 'vendor.json'), files) }
+    let(:entries) { ['somefile2.txt', 'somefile3', 'some/file', 'some/other/file', 'other'] }
+
+    it 'processes the vendor.json file' do
+      stub_request(:get, url1).to_return(:body => File.new(file1), :status => 200)
+      stub_request(:get, url2).to_return(:body => File.new(file2), :status => 200)
+      stub_request(:get, url3).to_return(:body => File.new(file3), :status => 200)
+      File.write(File.join(target, 'vendor.json'), files)
+     
+      # we should not have any errors
+      expect{ FileDependencies.process_vendor(target, 'vendor', tmpdir) }.to_not(raise_error)
+
+      # check if we got all the expected files
+      found_files = Dir.glob(File.join(target, 'vendor', '**', '*')).reject { |entry| File.directory?(entry) }.sort
+      expect_files = entries.map { |k| ::File.join(target, 'vendor', k) }.sort
+      expect(found_files).to(eq(expect_files))
+      
+    end
+
+  end
+
+end
