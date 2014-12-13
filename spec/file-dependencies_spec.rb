52278779bd421492f923202ff592d233cfebda27
Richard Pijnenburg <richard@ispavailability.com>
2014-12-13 12:44:56 +0000

    rename 'files' to 'extract' and add some tests

--- spec/file-dependencies_spec.rb
+++ spec/file-dependencies_spec.rb
@@ -7,7 +7,7 @@ describe FileDependencies do
 
   describe '.download' do
     after do
-      [tmpdir, target, file1, file2, file3].each do |entry|
+      [tmpdir, target, file1, file2, file3, file4].each do |entry|
         FileUtils.remove_entry_secure(entry)
       end
     end
@@ -18,23 +18,27 @@ describe FileDependencies do
     let(:file1) { Assist.generate_tarball({'some/file' => 'content1', 'some/other/file' => 'content2', 'other' => 'content3'}) }
     let(:file2) { Assist.generate_file('some_content') }
     let(:file3) { Assist.generate_gzip('some_content_for_gzip') }
+    let(:file4) { Assist.generate_tarball({'jars/some.jar' => 'content10', 'jars/someother.jar' => 'content11'}) }
 
     let(:sha1) { FileDependencies::File.calc_sha1(file1) }
     let(:sha2) { FileDependencies::File.calc_sha1(file2) }
     let(:sha3) { FileDependencies::File.calc_sha1(file3) }
+    let(:sha4) { FileDependencies::File.calc_sha1(file4) }
 
     let(:url1) { 'http://www.example.com/somefile1.tar.gz' }
     let(:url2) { 'http://www.example.com/somefile2.txt' }
     let(:url3) { 'http://www.example.com/somefile3.gz' }
+    let(:url4) { 'http://www.example.com/somefile4.tar.gz' }
 
-    let(:entries) { ['somefile2.txt', 'somefile3', 'some/file', 'some/other/file', 'other'] }
+    let(:entries) { ['somefile2.txt', 'somefile3', 'some/file', 'some/other/file', 'other', 'some.jar', 'someother.jar'] }
 
-    let(:files) { [ { 'url' => url1, 'sha1' => sha1 }, { 'url' => url2, 'sha1' => sha2 }, { 'url' => url3, 'sha1' => sha3 } ] }
+    let(:files) { [ { 'url' => url1, 'sha1' => sha1 }, { 'url' => url2, 'sha1' => sha2 }, { 'url' => url3, 'sha1' => sha3 }, { 'url' => url4, 'sha1' => sha4, 'extract' => '.jar' } ] }
 
     it 'processes file list' do
       stub_request(:get, url1).to_return(:body => File.new(file1), :status => 200)
       stub_request(:get, url2).to_return(:body => File.new(file2), :status => 200)
       stub_request(:get, url3).to_return(:body => File.new(file3), :status => 200)
+      stub_request(:get, url4).to_return(:body => File.new(file4), :status => 200)
      
       # we should not have any errors
       expect{ FileDependencies.download(files, target, tmpdir) }.to_not(raise_error)
@@ -49,7 +53,7 @@ describe FileDependencies do
 
   describe '.process_vendor' do
     after do
-      [tmpdir, target, file1, file2, file3].each do |entry|
+      [tmpdir, target, file1, file2, file3, file4].each do |entry|
         FileUtils.remove_entry_secure(entry)
       end
     end
@@ -60,23 +64,27 @@ describe FileDependencies do
     let(:file1) { Assist.generate_tarball({'some/file' => 'content1', 'some/other/file' => 'content2', 'other' => 'content3'}) }
     let(:file2) { Assist.generate_file('some_content') }
     let(:file3) { Assist.generate_gzip('some_content_for_gzip') }
+    let(:file4) { Assist.generate_tarball({'jars/some.jar' => 'content10', 'jars/someother.jar' => 'content11'}) }
 
     let(:sha1) { FileDependencies::File.calc_sha1(file1) }
     let(:sha2) { FileDependencies::File.calc_sha1(file2) }
     let(:sha3) { FileDependencies::File.calc_sha1(file3) }
+    let(:sha4) { FileDependencies::File.calc_sha1(file4) }
 
     let(:url1) { 'http://www.example.com/somefile1.tar.gz' }
     let(:url2) { 'http://www.example.com/somefile2.txt' }
     let(:url3) { 'http://www.example.com/somefile3.gz' }
+    let(:url4) { 'http://www.example.com/somefile4.tar.gz' }
 
-    let(:files) { [ { 'url' => url1, 'sha1' => sha1 }, { 'url' => url2, 'sha1' => sha2 }, { 'url' => url3, 'sha1' => sha3 } ].to_json }
+    let(:files) { [ { 'url' => url1, 'sha1' => sha1 }, { 'url' => url2, 'sha1' => sha2 }, { 'url' => url3, 'sha1' => sha3 }, { 'url' => url4, 'sha1' => sha4, 'extract' => '.jar' } ].to_json }
     let(:vendorfile) { File.write(File.join(target, 'vendor.json'), files) }
-    let(:entries) { ['somefile2.txt', 'somefile3', 'some/file', 'some/other/file', 'other'] }
+    let(:entries) { ['somefile2.txt', 'somefile3', 'some/file', 'some/other/file', 'other', 'some.jar', 'someother.jar'] }
 
     it 'processes the vendor.json file' do
       stub_request(:get, url1).to_return(:body => File.new(file1), :status => 200)
       stub_request(:get, url2).to_return(:body => File.new(file2), :status => 200)
       stub_request(:get, url3).to_return(:body => File.new(file3), :status => 200)
+      stub_request(:get, url4).to_return(:body => File.new(file4), :status => 200)
       File.write(File.join(target, 'vendor.json'), files)
      
       # we should not have any errors
