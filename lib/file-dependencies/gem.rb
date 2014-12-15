d4ae5f31f8e85da550301299650511c6957cf0ee
Richard Pijnenburg <richard@ispavailability.com>
2014-12-15 13:40:33 +0000

    Fix Gem hook

--- lib/file-dependencies/gem.rb
+++ lib/file-dependencies/gem.rb
@@ -1,9 +1,10 @@
 require 'json'
+require 'file-dependencies'
 module FileDependencies
   # :nodoc:
   module Gem
     def hook
-      Gem.post_install do |gem_installer|
+      ::Gem.post_install do |gem_installer|
         next if ENV['VENDOR_SKIP'] == 'true'
         FileDependencies.process_vendor(gem_installer.gem_dir)
       end
