aa23a938cec6da9d67106931924903da5e36c35a
Richard Pijnenburg <richard@ispavailability.com>
2015-01-06 13:10:15 +0000

    Version bump to 0.1.3

--- lib/file-dependencies/version.rb
+++ lib/file-dependencies/version.rb
@@ -1,5 +1,5 @@
 # Note to authors: this should not include dashes because 'gem' barfs if
 # you include a dash in the version string.
 module FileDependencies
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 end
