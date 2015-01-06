f0ee6d4f40f625bf1d242034868fe74ee4db4d76
Richard Pijnenburg <richard@ispavailability.com>
2015-01-06 16:57:58 +0000

    Make sure of ruby's path separator constant

--- lib/file-dependencies.rb
+++ lib/file-dependencies.rb
@@ -24,10 +24,10 @@ module FileDependencies
       download = FileDependencies::File.fetch_file(file['url'], file['sha1'], tmpdir)
       if (res = download.match(/(\S+?)(\.tar\.gz|\.tgz)$/))
         FileDependencies::Archive.untar(download) do |entry|
-          prefix = file['include_tar_prefix'] ? '' : ::File.join(entry.full_name.split("/").first, '')
+          prefix = file['include_tar_prefix'] ? '' : ::File.join(entry.full_name.split(::File::SEPARATOR).first, '')
           next unless FileDependencies::Archive.extract_file?(entry.full_name, file['extract'], file['exclude'], prefix)
           if file['flatten'] == true
-            ::File.join(full_target, entry.full_name.split("/").last)
+            ::File.join(full_target, entry.full_name.split(::File::SEPARATOR).last)
           else
             ::File.join(full_target, entry.full_name.gsub(prefix, ''))
           end
@@ -35,7 +35,7 @@ module FileDependencies
       elsif download =~ /.gz$/
         FileDependencies::Archive.ungzip(download, full_target)
       else
-        FileUtils.mv(download, ::File.join(full_target, download.split("/").last))
+        FileUtils.mv(download, ::File.join(full_target, download.split(::File::SEPARATOR).last))
       end
     end
   end # def download
